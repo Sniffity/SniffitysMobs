1a46f28c134867a8bef95ac07469db7abdec0d8a
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-18 17:24:27 -0500

    Configure project properties

--- src/main/java/com/github/sniffity/sniffitysmobs/SniffitysMobs.java
+++ src/main/java/com/github/sniffity/sniffitysmobs/SniffitysMobs.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.github.sniffity.sniffitysmobs;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -19,12 +19,12 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("examplemod")
-public class ExampleMod
+public class SniffitysMobs
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public SniffitysMobs()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
