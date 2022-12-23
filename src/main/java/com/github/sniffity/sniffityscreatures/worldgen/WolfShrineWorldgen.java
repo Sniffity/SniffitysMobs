4d0b6e10fbe26969401fb8ef0c157238cf63ef87
Sniffity <66455728+Sniffity@users.noreply.github.com>
2022-12-23 14:01:54 -0500

    WorldGen: Add new pieces + terrain matching projection

--- src/main/java/com/github/sniffity/sniffityscreatures/worldgen/WolfShrineWorldgen.java
+++ src/main/java/com/github/sniffity/sniffityscreatures/worldgen/WolfShrineWorldgen.java
@@ -29,19 +29,16 @@ import java.util.stream.Collectors;
  */
 
 public class WolfShrineWorldgen {
-    private static final ResourceLocation wolfShrineStructurePlains = new ResourceLocation("sniffityscreatures", "village/wolf_shrine_plains");
-    private static final ResourceLocation wolfShrineStructureTaiga = new ResourceLocation("sniffityscreatures", "village/wolf_shrine_taiga");
-    private static final ResourceLocation wolfShrineStructureSavanna = new ResourceLocation("sniffityscreatures", "village/wolf_shrine_savanna");
-    private static final ResourceLocation wolfShrineStructureDesert = new ResourceLocation("sniffityscreatures", "village/wolf_shrine_desert");
-    private static final ResourceLocation wolfShrineStructureSnowy = new ResourceLocation("sniffityscreatures", "village/wolf_shrine_snowy");
 
     public static void setupVillageWorldGen() {
         if (SMServerConfig.SERVER.ENTITIES.WEREWOLF.enableWerewolf.get()) {
-            addToPool(new ResourceLocation("village/plains/streets"),wolfShrineStructurePlains, SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightPlains.get());
-            addToPool(new ResourceLocation("village/taiga/streets"),wolfShrineStructureTaiga, SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightTaiga.get());
-            addToPool(new ResourceLocation("village/savanna/streets"),wolfShrineStructureSavanna, SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightSavanna.get());
-            addToPool(new ResourceLocation("village/desert/streets"),wolfShrineStructureDesert, SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightDesert.get());
-            addToPool(new ResourceLocation("village/snowy/streets"),wolfShrineStructureSnowy, SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightSnowy.get());
+            for (int i = 0; i<3; i++){
+                addToPool(new ResourceLocation("village/plains/streets"),new ResourceLocation("sniffityscreatures", "village/wolf_shrine_plains"+i), SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightPlains.get());
+                addToPool(new ResourceLocation("village/taiga/streets"),new ResourceLocation("sniffityscreatures", "village/wolf_shrine_taiga"+i), SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightTaiga.get());
+                addToPool(new ResourceLocation("village/savanna/streets"),new ResourceLocation("sniffityscreatures", "village/wolf_shrine_savanna"+i), SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightSavanna.get());
+                addToPool(new ResourceLocation("village/desert/streets"),new ResourceLocation("sniffityscreatures", "village/wolf_shrine_desert"+i), SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightDesert.get());
+                addToPool(new ResourceLocation("village/snowy/streets"),new ResourceLocation("sniffityscreatures", "village/wolf_shrine_snowy"+i), SMServerConfig.SERVER.ENTITIES.WEREWOLF.wolfShrineSpawnWeightSnowy.get());
+            }
         }
     }
 
@@ -60,7 +57,7 @@ public class WolfShrineWorldgen {
         for(StructurePoolElement p : shuffled)
             newPieces.computeInt(p, (StructurePoolElement pTemp, Integer i) -> (i==null?0: i)+1);
         newPieces.put(MixinSingleJigsawAccess.construct(
-                Either.left(toAdd), ProcessorLists.EMPTY, StructureTemplatePool.Projection.RIGID
+                Either.left(toAdd), ProcessorLists.EMPTY, StructureTemplatePool.Projection.TERRAIN_MATCHING
         ), weight);
         List<Pair<StructurePoolElement, Integer>> newPieceList = newPieces.object2IntEntrySet().stream()
                 .map(e -> Pair.of(e.getKey(), e.getIntValue()))
